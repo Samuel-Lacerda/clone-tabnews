87f4da3ef590908c49b3d93f58480ece869af5af
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-11-04 22:25:13 +0000

    adiciona um target _blank

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home (){
         <div>
             <h1>Manozita</h1>
             <p>Tu és pra mim um chuchu que colhi na vida</p>
-            <a href="https://www.youtube.com/watch?v=VTNen48uCKA">Clica aqui</a>
+            <a href="https://www.youtube.com/watch?v=VTNen48uCKA" target="_blank">Clica aqui</a>
         </div>
     )
 }
