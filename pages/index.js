4d00fb35f394f9c1525368dc749f38dc803b92a2
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-11-04 22:23:01 +0000

    Alterei a mensagem para minha amada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home (){
-    return <h1>Uma frase bem impactante!</h1>
+    return(
+        <div>
+            <h1>Manozita</h1>
+            <p>Tu és pra mim um chuchu que colhi na vida</p>
+            <a href="https://www.youtube.com/watch?v=VTNen48uCKA">Clica aqui</a>
+        </div>
+    )
 }
 
 export default Home;
