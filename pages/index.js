778c38675c25fd10dd4f30368aa3c7f502847c9a
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-11-06 09:01:42 +0000

    Adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
-import styles from './home.module.css'
+import styles from "./home.module.css";
 
 function Home() {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Manozita</h1>
-            <p className={styles.text}>Tu és pra mim um chuchu que colhi na vida ❤️</p>
-            <div className={styles.container2}>
-                <a href="https://www.youtube.com/watch?v=VTNen48uCKA" target="_blank" className={styles.link}>Clica aqui</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Manozita</h1>
+      <p className={styles.text}>
+        Tu és pra mim um chuchu que colhi na vida ❤️
+      </p>
+      <div className={styles.container2}>
+        <a
+          href="https://www.youtube.com/watch?v=VTNen48uCKA"
+          target="_blank"
+          className={styles.link}
+        >
+          Clica aqui
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
